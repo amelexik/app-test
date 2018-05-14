191b9915a33ced8a52371ac397f72314b6264955
amelex <amelexik@gmail.com>
2018-05-14 23:30:46 +0300

    Update Readme

--- dump.sql
+++ dump.sql
@@ -1,17 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
+-- version 4.4.2
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Час створення: Трв 14 2018 р., 20:39
--- Версія сервера: 5.7.21-0ubuntu0.16.04.1
--- Версія PHP: 7.0.28-0ubuntu0.16.04.1
+-- Час створення: Трв 14 2018 р., 23:36
+-- Версія сервера: 5.5.44-0+deb7u1
+-- Версія PHP: 5.6.25-1~dotdeb.7.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- База даних: `test-app.local`
+-- База даних: `melnik`
 --
 
 -- --------------------------------------------------------
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 -- Структура таблиці `comments`
 --
 
-CREATE TABLE `comments` (
+CREATE TABLE IF NOT EXISTS `comments` (
   `comment_id` int(11) NOT NULL,
   `pid` int(11) DEFAULT NULL,
   `user` int(11) NOT NULL,
@@ -28,17 +28,23 @@ CREATE TABLE `comments` (
   `rating` tinyint(4) NOT NULL,
   `url` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '/',
   `text` varchar(1000) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Дамп даних таблиці `comments`
 --
 
 INSERT INTO `comments` (`comment_id`, `pid`, `user`, `created`, `rating`, `url`, `text`) VALUES
-(17, NULL, 1, '2018-05-14 15:27:06', 0, '/', 'fdsfdsf'),
-(18, NULL, 1, '2018-05-14 15:27:12', 0, '/', 'fdsfdsf'),
-(19, NULL, 1, '2018-05-14 15:30:15', 0, '/', 'Ð¾Ð»Ð¾Ð»Ð¾'),
-(20, NULL, 1, '2018-05-14 15:30:20', 0, '/', 'Ð¾Ð»Ð¾Ð»Ð¾');
+(1, NULL, 2, '2018-05-14 20:31:52', 0, '/', '"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"'),
+(2, NULL, 2, '2018-05-14 20:32:03', 0, '/', '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"'),
+(3, NULL, 2, '2018-05-14 20:32:21', 0, '/', 'Битовая маска, составляемая из значений JSON_HEX_QUOT, JSON_HEX_TAG, JSON_HEX_AMP, JSON_HEX_APOS, JSON_NUMERIC_CHECK, JSON_PRETTY_PRINT, JSON_UNESCAPED_SLASHES, JSON_FORCE_OBJECT, JSON_PRESERVE_ZERO_FRACTION, JSON_UNESCAPED_UNICODE, JSON_PARTIAL_OUTPUT_ON_ERROR. Смысл этих констант объясняется на странице JSON констант.'),
+(4, NULL, 2, '2018-05-14 20:32:42', 0, '/', 'note the return (confirm): the value returned by scripts in intrinsic evens decides whether the default browser action is run or not; in case you need to run a big piece of code you can of course call another function:'),
+(5, NULL, 1, '2018-05-14 20:33:07', 0, '/', 'Многие думают, что Lorem Ipsum - взятый с потолка псевдо-латинский набор слов, но это не совсем так. Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum, "consectetur", и занялся его поисками в классической латинской литературе. В результате он нашёл неоспоримый первоисточник Lorem Ipsum в разделах 1.10.32 и 1.10.33 книги "de Finibus Bonorum et Malorum" ("О пределах добра и зла"), написанной Цицероном в 45 году н.э. Этот трактат по теории этики был очень популярен в эпоху Возрождения. Первая строка Lorem Ipsum, "Lorem ipsum dolor sit amet..", происходит от одной из строк в разделе 1.10.32'),
+(6, NULL, 1, '2018-05-14 20:33:26', 0, '/', 'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.." Многие программы электронной вёрстки и редакторы HTML используют Lorem Ipsum в качестве текста по умолчанию, так что поиск по ключевым словам "lorem ipsum" сразу показывает, как много веб-страниц всё ещё дожидаются своего настоящего рождения. За прошедшие годы текст Lorem Ipsum получил много версий. Некоторые версии появились по ошибке, некоторые - намеренно (например, юмористические варианты).'),
+(7, NULL, 2, '2018-05-14 20:33:53', 0, '/', 'There are a number of mailing lists devoted to talking about PHP and related projects. This list describes them all, has links to searchable archives for all of the lists, and explains how to subscribe to the lists.'),
+(8, 6, 2, '2018-05-14 20:34:42', 0, '/', 'Coool!'),
+(9, 8, 1, '2018-05-14 20:35:26', 0, '/', 'No problem1 bro!'),
+(10, 9, 2, '2018-05-14 20:36:00', 0, '/', 'Chances are that there is a User Group in your neighborhood, which are generally a great resource both for beginners and experienced PHP users. Check out the User Group listing on PHP.ug to see if there is one close by.');
 
 -- --------------------------------------------------------
 
@@ -46,12 +52,12 @@ INSERT INTO `comments` (`comment_id`, `pid`, `user`, `created`, `rating`, `url`,
 -- Структура таблиці `users`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL,
   `login` varchar(20) CHARACTER SET utf8 NOT NULL,
   `password` varchar(32) CHARACTER SET utf8 NOT NULL,
   `status` tinyint(4) NOT NULL DEFAULT '1'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Дамп даних таблиці `users`
@@ -59,7 +65,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `login`, `password`, `status`) VALUES
 (1, 'user', '5f4dcc3b5aa765d61d8327deb882cf99', 1),
-(2, 'blogger', '827ccb0eea8a706c4c34a16891f84e7b', 1);
+(2, 'root', '827ccb0eea8a706c4c34a16891f84e7b', 1);
 
 --
 -- Індекси збережених таблиць
@@ -86,12 +92,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблиці `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT для таблиці `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Обмеження зовнішнього ключа збережених таблиць
 --
